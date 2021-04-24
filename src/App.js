edc14b419b53295ec909fc2b77a5eaa822a75b4f
benberryallwood <b.berry-allwood@protonmail.com>
2021-04-24 14:29:37 +0000

    Do most of the functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Markdown Previewer</h1>
-      </header>
-    </div>
-  );
+const Editor = (props) => {
+    return (
+        <div>
+            <textarea id="editor" onChange={props.handleChange} value={props.content} />
+        </div>
+    );
+}
+
+const Preview = (props) => {
+    return (
+        <div id="preview">
+            <p>{props.content}</p>
+        </div>
+    );
+};
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            content: 'markdown'
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            content: event.target.value
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>Markdown Previewer</h1>
+                    <Editor content={this.state.content} handleChange={this.handleChange} />
+                    <Preview content={this.state.content} />
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
